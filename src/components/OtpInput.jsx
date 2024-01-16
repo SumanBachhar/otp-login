d7db512cd67016584ffdfa98bfb3f9d013198bca
Suman-199 <suman.mmt199@gmail.com>
2024-01-16 22:57:45 +0530

    otp input field is completed

--- src/components/OtpInput.jsx
+++ src/components/OtpInput.jsx
@@ -1,3 +1,74 @@
-export const OtpInput = () => {
-  return <div>OtpInput</div>;
+import { useEffect, useRef, useState } from "react";
+
+export const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
+  const [otp, setOtp] = useState(new Array(length).fill(""));
+
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
+
+  console.log(inputRefs);
+
+  const changeHandler = (index, e) => {
+    const value = e.target.value;
+    if (isNaN(value)) {
+      return;
+    }
+    const newOtp = [...otp];
+    // alllow only one input value
+    newOtp[index] = value.substring(value.length - 1);
+    setOtp(newOtp);
+    // submit Triger values
+    const combineOtp = newOtp.join("");
+    if (combineOtp.length === length) onOtpSubmit(combineOtp);
+
+    // Move to next input if current field is filled
+    if (value && index < length - 1 && inputRefs.current[index + 1]) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const clickHandler = (index) => {
+    inputRefs.current[index].setSelectionRange(1, 1);
+
+    // optional
+    if (index > 0 && !otp[index - 1]) {
+      inputRefs.current[otp.indexOf("")].focus();
+    }
+  };
+
+  const keyDownHandler = (index, e) => {
+    if (
+      e.key === "Backspace" &&
+      !otp[index] &&
+      index > 0 &&
+      inputRefs.current[index - 1]
+    ) {
+      // Move focus to the previous input field on backspace
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  return (
+    <>
+      {otp.map((value, index) => {
+        return (
+          <input
+            type="text"
+            className="otpInput"
+            key={index}
+            value={value}
+            ref={(input) => (inputRefs.current[index] = input)}
+            onChange={(e) => changeHandler(index, e)}
+            onClick={() => clickHandler(index)}
+            onKeyDown={(e) => keyDownHandler(index, e)}
+          />
+        );
+      })}
+    </>
+  );
 };
--- src/components/PhoneLoginForm.jsx
+++ src/components/PhoneLoginForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { OtpInput } from "./OtpInput";
 
 export const PhoneLoginForm = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [showInput, setShowInput] = useState(false);
 
   const phoneNumberHandler = (event) => {
     setPhoneNumber(event.target.value);
@@ -9,7 +11,7 @@ export const PhoneLoginForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(phoneNumber);
+    // console.log(phoneNumber);
 
     // phone validations
     const regex = /[^0-9]/g;
@@ -18,20 +20,32 @@ export const PhoneLoginForm = () => {
       return;
     }
 
-    setPhoneNumber("");
+    // otp field
+    setShowInput(true);
+  };
+
+  const onOtpSubmit = (otp) => {
+    console.log("Login Successful", otp);
   };
 
   return (
     <>
-      <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          value={phoneNumber}
-          onChange={phoneNumberHandler}
-          placeholder="Enter your phone number"
-        />
-        <button type="submit">Submit</button>
-      </form>
+      {!showInput ? (
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={phoneNumberHandler}
+            placeholder="Enter your phone number"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div>
+          <p>Enter OTP sent to {phoneNumber}</p>
+          <OtpInput length={4} onOtpSubmit={onOtpSubmit} />
+        </div>
+      )}
     </>
   );
 };
